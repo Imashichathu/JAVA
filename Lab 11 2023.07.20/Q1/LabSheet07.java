3d1cb3ccaeea3f60dc1f6d659894359e2058a297
Imashichathu <131977756+Imashichathu@users.noreply.github.com>
2023-07-22 09:50:08 +0530

    Add files via upload

---
+++ Lab 11 2023.07.20/Q1/LabSheet07.java
@@ -0,0 +1,9 @@
+package com.mycompany.labsheet07;
+public class LabSheet07
+{
+    public static void main(String[] args)
+    {
+        Example e = new Example();
+        e.show();
+    }
+}
---
+++ Lab 11 2023.07.20/Q2/LabSheet08.java
@@ -0,0 +1,9 @@
+package com.mycompany.labsheet08;
+public class LabSheet08
+{
+    public static void main(String[] args)
+    {
+        Example2 e = new Example2();
+        e.show();
+    }
+}
